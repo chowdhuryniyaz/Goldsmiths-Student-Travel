e52c17a74722cf6e9a8957e70e630db915fbc508
Dora B Kiss <dkiss001@gold.ac.uk>
2018-11-14 22:18:02 +0000

    changed pie charts to donut

--- src/gender_donut_chart.py
+++ src/gender_donut_chart.py
@@ -20,17 +20,23 @@ fig, ax = plt.subplots(figsize=(6, 4))
 # create pie chart from gender column
 ax.pie(df.gender.value_counts(),
         colors=colours,
+        labels=df.gender.cat.categories,
         startangle=90,
-        autopct='%.0f%%')
+        wedgeprops = {'linewidth': 0.5, 'edgecolor' : 'lightgrey', 'width':0.5},
+        textprops={'size': 'small', 'weight':'heavy'},
+        autopct='%.0f%%',
+        pctdistance=0.75, 
+        labeldistance=1.1)
 
 # Equal aspect ratio ensures that pie is drawn as a circle.
 ax.axis('equal')
 plt.subplots_adjust(bottom=0.1)
 
 # Put a legend below current axis
-plt.legend(df.gender.cat.categories,bbox_to_anchor=(0.5,0),loc="lower center", bbox_transform=fig.transFigure, ncol=2)
-plt.title("Gender split")
+#plt.legend(df.gender.cat.categories,bbox_to_anchor=(0.5,0),loc="lower center", bbox_transform=fig.transFigure, ncol=2)
+plt.title("Gender split", pad=30, weight='heavy')
 plt.tight_layout()
+
+# to save the chart as a png image, uncomment the line below
+#plt.savefig("gender_split_pie.png", bbox_inches="tight", dpi=200)
 plt.show()
-# to save the chart as a ong image, uncomment the line below
-#plt.savefig("gender_split_pie.png", bbox_inches="tight")
---
+++ src/mode_of_transport_donut.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pandas.api.types import CategoricalDtype
+from loadData import *
+
+path = './data/survey_responses.csv'
+
+df = loadData(path)
+df = convertDataType(df)
+colours = ['#ffa600', '#ff6361', '#bc5090', '#58508d', '#003f5c']
+fig, ax = plt.subplots(figsize=(6, 4))
+# create donut chart from mode_of_transport column
+ax.pie(df.mode_of_transport.value_counts(),
+        colors=colours,
+        labels=df.mode_of_transport.cat.categories,
+        startangle=90,
+        wedgeprops = {'linewidth': 0.5, 'edgecolor' : 'lightgrey', 'width':0.5},
+        textprops={'size': 'small', 'weight':'heavy'},
+        autopct='%.0f%%',
+        pctdistance=0.75, 
+        labeldistance=1.1)
+
+# add title
+plt.title("Main mode of transport", weight='heavy', pad=30)
+
+plt.tight_layout()
+# to save the chart as a png image, uncomment the line below
+#plt.savefig("mode_of_transport_pie.png", bbox_inches="tight")
+plt.show()
