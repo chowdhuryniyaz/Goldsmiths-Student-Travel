cb144539fe8a2a7255dfbc3d4f4a70707af67177
Dora B Kiss <dkiss001@gold.ac.uk>
2018-11-14 23:31:41 +0000

    add region chart

--- src/region_chaart.py
+++ src/region_chaart.py
@@ -12,15 +12,16 @@ df = loadData(path)
 df = convertDataType(df)
 
 # respondents who refused an answer are not included in the analysis 
-df.gender.cat.remove_categories('I would prefer not to say', inplace=True)
-colours = ['#ffa600', '#ff6361', '#bc5090', '#58508d']
+df.region.cat.remove_categories('other', inplace=True)
+
+colours = ['#bc5090', '#003f5c', '#ffa600']
 
 fig, ax = plt.subplots(figsize=(6, 4))
 
 # create pie chart from gender column
-patches, texts, autotexts = ax.pie(df.gender.value_counts(),
+patches, texts, autotexts = ax.pie(df.region.value_counts(),
         colors=colours,
-        labels=df.gender.cat.categories,
+        labels=df.region.cat.categories,
         startangle=50,
         wedgeprops = {'linewidth': 0.5, 'edgecolor' : 'lightgrey', 'width':0.5},
         textprops={'weight':'heavy'},
@@ -30,14 +31,15 @@ patches, texts, autotexts = ax.pie(df.gender.value_counts(),
 
 for t in autotexts:
         t.set_fontsize(14)
-        
+
+
 # Equal aspect ratio ensures that pie is drawn as a circle.
 ax.axis('equal')
 plt.subplots_adjust(bottom=0.1)
 
 # Put a legend below current axis
 #plt.legend(df.gender.cat.categories,bbox_to_anchor=(0.5,0),loc="lower center", bbox_transform=fig.transFigure, ncol=2)
-plt.title("Gender split", pad=30, weight='heavy')
+plt.title("Region split", pad=30, weight='heavy')
 plt.tight_layout()
 
 # to save the chart as a png image, uncomment the line below
