8bc00b2f866a7efe3d030f7ba773b92c6c151566
Dora B Kiss <dkiss001@gold.ac.uk>
2018-11-15 18:12:57 +0000

    add age chart

---
+++ src/age_bar_chart.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pandas.api.types import CategoricalDtype
+from loadData import *
+from textwrap import wrap
+
+path = './data/survey_responses.csv'
+
+# load and preprocess data
+df = loadData(path)
+df = convertDataType(df)
+
+# respondents who refused an answer are not included in the analysis 
+df = df[df['age'] != 'I would prefer not to say']
+df.age.cat.remove_categories('I would prefer not to say', inplace=True)
+
+# count the frequency for each category
+counts = df['age'].value_counts(sort=False)
+
+# calcuate percentages from counts
+pct = counts / counts.sum() * 100
+
+fig, ax = plt.subplots()
+colours=['#4D962E', '#70AD56', '#90CC76', '#B8EFA0', '#D2FFBF']
+
+
+pct.plot(kind='barh',
+ax = ax,
+color=colours,
+rot=0)
+ax.set_xlabel('%')
+ax.set_ylabel('Age')
+plt.title("Age of respondents", pad=20, weight='heavy')
+plt.tight_layout()
+plt.show()
---
+++ src/mode_of_study_donut.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from pandas.api.types import CategoricalDtype
+from loadData import *
+
+path = './data/survey_responses.csv'
+
+#load and preprocess data
+df = loadData(path)
+df = convertDataType(df)
+
+fig, ax = plt.subplots(figsize=(6, 4))
+# Set aspect ratio to be equal so that pie is drawn as a circle
+ax.axis('equal')
+width = 0.3
+
+# use the colormap in Matplotlib
+cm = plt.get_cmap("tab20c")
+
+# inside ring
+cin = cm(np.arange(3)*4)
+labels_in = df.mode_of_study.cat.categories
+#labels_in = ['FT', 'PT', 'o']
+pie, _ = ax.pie(df['mode_of_study'].value_counts(sort=False), radius=1-width,colors=cin)
+plt.setp(pie, width=width, edgecolor='white')
+plt.legend(pie, labels_in, loc="upper left", bbox_to_anchor=(1, 0, 0.5, 1))
+
+# outside ring
+cout = cm(np.array([1,2,3,6,7,10]))
+labels_out = df.level_of_study.cat.categories
+wedges, texts = pie2, _ = ax.pie(df['level_of_study'].value_counts(sort=False), radius=1, colors=cout)
+plt.setp(pie2, width=width, edgecolor='white')
+
+
+data = df['level_of_study'].value_counts(sort=False)
+bbox_props = dict(boxstyle="square,pad=0.3", fc="w", ec="k", lw=0.72)
+kw = dict(xycoords='data', textcoords='data',  arrowprops=dict(arrowstyle="-"), bbox=bbox_props, va="center")
+
+for i, p in enumerate(wedges):
+    ang = (p.theta2 - p.theta1)/2. + p.theta1
+    y = np.sin(np.deg2rad(ang))
+    x = np.cos(np.deg2rad(ang))
+    horizontalalignment = {-1: "right", 1: "left"}[int(np.sign(x))]
+    connectionstyle = "angle,angleA=0,angleB={}".format(ang)
+    kw["arrowprops"].update({"connectionstyle": connectionstyle})
+    ax.annotate(labels_out[i], xy=(x, y),xytext=(1.15*np.sign(x), 1.4*y),
+             horizontalalignment=horizontalalignment, **kw)
+plt.subplots_adjust(left=0.3)
+plt.tight_layout()
+plt.show()
