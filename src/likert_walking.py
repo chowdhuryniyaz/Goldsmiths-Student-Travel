191db18e23a7c06f2a5a6370897da9b254e7377f
Dora B Kiss <dkiss001@gold.ac.uk>
2018-11-18 20:33:56 +0000

    fixed position of legend and order of labels

--- src/likert_walking.py
+++ src/likert_walking.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from pandas.api.types import CategoricalDtype
 from loadData import *
+from textwrap import wrap
 
 path = './data/survey_responses.csv'
 
@@ -29,12 +30,18 @@ df = df.rename(columns = {'pedestrian_crossings': 'Not enough pedestrian crossin
 
 df = df.melt(var_name = 'Statement', value_name = 'Answer')
 table = pd.crosstab(df['Statement'], df['Answer'], normalize = 'index') * 100
+table = table[['Strongly disagree', 'Somewhat disagree', 'Neither agree nor disagree', 'Somewhat agree', 'Strongly agree']]
 
 colours = ['#004c6d', '#247392', '#449cb7', '#66c7dc', '#8bf3ff']
-ax = table.plot.barh(color = colours, rot = 0, stacked = True)
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, borderaxespad=0.)
+ax = table.plot.barh(color = colours, rot = 0, stacked = True, figsize=(8,4))
+
+handles, labels = ax.get_legend_handles_labels()
+labels = [ '\n'.join(wrap(l, 14)) for l in labels ]
+ax.legend(handles, labels, bbox_to_anchor=(1.0, 0.75), frameon=False)
+
 ax.set_xlabel('Percentage %')
 ax.set_ylabel('Statements')
+plt.xticks([0, 20, 40, 60, 80, 100])
 plt.title("To what extent do reponders agree with the following", pad=20, weight='heavy')
 plt.tight_layout()
 plt.show()
